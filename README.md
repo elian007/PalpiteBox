9174de390f8b3116666e5c19e37891638064ca51
Elian07 <elians.2016@alunos.utfpr.edu.br>
2020-07-08 20:28:46 -0300

    Adicionando Readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# PalpiteBox
-Projeto da semana Fullstack Master DevPleno
+# PalpiteBox - Semana Fullstack Master
+
+
+Este projeto foi construído durante a Semana Fullstack Master do [DevPleno](https://devpleno.com). 
+
+## Início
+
+### Pré-requisitos:
+
+Você precisa do NodeJS e do NPM instalado em sua máquina.
+
+```
+npm install
+npm run dev
+```
+
+## Layout:
+
+Criamos o layout utilizando o Figma. Você pode encontrar o arquivo [aqui](https://www.figma.com/file/HxvAYhS6l7UDI49u8uLdaC/palpite-box?node-id=0%3A1).
+
+## Colocando em produção:
+
+
+## Construído com:
+
+* [NextJS](https://nextjs.org/) -The React Framework.
+* [TailwindCSS](https://tailwindcss.com/) - A utility-first CSS framework for
+rapidly building custom designs.
+* [Figma](https://figma.com/) - Online prototyping tool.
+* [Google Sheets](https://drive.google.com) - Planilhas online do Google
+
+## Author:
+
+* **Elian Marcos** - [LinkedIn](https://www.linkedin.com/in/elian-marcos/)
+
+
+## Acknowledgments
+
+* Este projeto foi construído durante a Semana Fullstack Master do [DevPleno](https://devpleno.com).
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,26 +5,39 @@ const Footer = () => {
     <div className="bg-gray-700 p-4">
       <div className="container mx-auto text-center font-bold text-white">
         Projeto Desenvolvido por:
-        <a className="px-2 hover:underline" href="https://www.facebook.com/elian.marcos.7" target='_blank'>
+        <a
+          className="px-2 hover:underline"
+          href="https://www.facebook.com/elian.marcos.7"
+          target="_blank"
+        >
           {" "}
           Elian Marcos
         </a>{" "}
         /
         <a
           className="px-2 hover:underline"
-          href="https://linkedin.com/in/elian-marcos/" target='_blank'
+          href="https://linkedin.com/in/elian-marcos/"
+          target="_blank"
         >
           {" "}
           Linkedin
         </a>{" "}
         /
-        <a className="px-2 hover:underline" href="https://github.com/Elian07" target='_blank'>
+        <a
+          className="px-2 hover:underline"
+          href="https://github.com/Elian07"
+          target="_blank"
+        >
           {" "}
           GitHub
         </a>{" "}
         /
-        <div className='mt-4 '>
-          <img className="inline p-4 max-w-1/15" src="/logo_devpleno.png" alt="DevPleno" />
+        <div className="mt-4 ">
+          <img
+            className="inline p-4 max-w-1/15"
+            src="/logo_devpleno.png"
+            alt="DevPleno"
+          />
           <img
             className="inline p-4 max-w-1/15"
             src="/logo_semana_fsm.png"
