ac5002f8a3a8893d4dce22988d9ea0e4154f8693
Elian07 <elians.2016@alunos.utfpr.edu.br>
2020-07-11 20:58:25 -0300

    Adc base64

--- pages/contato.js
+++ pages/contato.js
@@ -9,16 +9,6 @@ const Contato = () => {
       <div>
         <span className="text-center">Entre em contato conosco, <br /> em alguma de nossa redes sociais!</span>
         <div className="container mx-auto text-center">
-          <Link href="">
-            <a>
-              <img
-                className="max-w-1/4 mx-auto"
-                src="/whats.png"
-                alt="Whatsapp"
-              />
-              <span className='hover:underlins'>(99) 9999 - 8888 </span>
-            </a>
-          </Link>
         </div>
       </div>
     </div>
